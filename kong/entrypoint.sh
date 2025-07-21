530d261c42f2bda36b03bb56df1e3f15b6e7db13
Matheus Vinicius <matheussvini@outlook.com>
2025-07-21 08:04:49 -0300

    fix: interpole kong env variables

--- kong/entrypoint.sh
+++ kong/entrypoint.sh
@@ -1,6 +1,10 @@
+# kong/entrypoint.sh
 #!/bin/sh
 set -e
 
-envsubst < /etc/kong/kong.yml > /tmp/kong.yml
+# Lê o template de /tmp/kong.yml.template e escreve o resultado em /etc/kong/kong.yml
+# Isso garante que o KONG_DECLARATIVE_CONFIG aponte para o arquivo já processado.
+envsubst < /tmp/kong.yml.template > /etc/kong/kong.yml
 
+# Inicia o Kong
 exec kong start
