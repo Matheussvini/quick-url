778d3d1fcc4361b7f60d69d52bcaa32ae6bde709
Matheus Vinicius <matheussvini@outlook.com>
2025-07-21 06:23:14 -0300

    fix: interpole kong env variables

--- kong/entrypoint.sh
+++ kong/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# Substitui os placeholders do kong.yml pelas variáveis de ambiente
-envsubst < /etc/kong/kong.yml > /etc/kong/kong.generated.yml
+# Substitui variáveis de ambiente no kong.yml
+envsubst < /etc/kong/kong.yml > /tmp/kong.yml
 
-# Inicia o Kong com o novo arquivo gerado
-exec kong start --conf /etc/kong/kong.conf.default --vv --declarative-config=/etc/kong/kong.generated.yml
+# Define variáveis necessárias para modo DB-less
+export KONG_DATABASE=off
+export KONG_DECLARATIVE_CONFIG=/tmp/kong.yml
+
+# Inicia o Kong
+exec kong start
